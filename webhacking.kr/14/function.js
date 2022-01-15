0f776b384c0d967b02c447f00d6f02949a63b17e
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-01-16 00:55:17 +0900

    Create function.js

---
+++ webhacking.kr/14/function.js
@@ -0,0 +1,7 @@
+function ck(){
+  var ul=document.URL; 
+  ul=ul.indexOf(".kr"); 
+  ul=ul*30; 
+  if(ul==pw.input_pwd.value) { location.href="?"+ul*pw.input_pwd.value; } 
+  else { alert("Wrong"); }
+}
