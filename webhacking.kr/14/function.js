0c7bc2f586bb661ea32952e79697382515df815b
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-01-15 23:46:54 +0900

    Update function.js

--- webhacking.kr/14/function.js
+++ webhacking.kr/14/function.js
@@ -1,7 +1,7 @@
 function ck(){
-  var ul=document.URL;
-  ul=ul.indexOf(".kr");
-  ul=ul*30;
-  if(ul==pw.input_pwd.value) { location.href="?"+ul*pw.input_pwd.value; }
+  var ul=document.URL; // document.URL은 문서의 URL을 반환하는 속성을 말한다.
+  ul=ul.indexOf(".kr"); // indexOf() 함수는 문자열에서 원하는 문자열을 검색하는 내장함수로 위치값을 index로 반환한다.
+  ul=ul*30; // ul * 30을 한다.
+  if(ul==pw.input_pwd.value) { location.href="?"+ul*pw.input_pwd.value; } // ul의 값이 pw.input_pwd.value값과 일치하면 if문이 실행된다.
   else { alert("Wrong"); }
 }
